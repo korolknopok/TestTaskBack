5b28f7a70a9e783280db50c2c9833e8e7c4df33f
Назмутдинов Артём <94155787+korolknopok@users.noreply.github.com>
2025-06-22 17:01:24 +0700

    refactor(task4.1-optimisation-reading-time)
    * refactor(task4-optimisation-reading-time): optimisation max and min filter
    
    * feature(task4-optimisation-reading-time): add index for column ReadingTime

---
+++ MCC.TestTask.Persistence/Migrations/20250622092305_AddReadingTimeIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MCC.TestTask.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReadingTimeIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_ReadingTime",
+                table: "Posts",
+                column: "ReadingTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_ReadingTime",
+                table: "Posts");
+        }
+    }
+}
